1185a25144fda6e9eeeb5f3551023d891983ad22
Slava <slava91petrushin@yandex.ru>
2020-08-31 09:35:59 +0300

    added instance for url

--- src/api/fetchWeatherCityApi.ts
+++ src/api/fetchWeatherCityApi.ts
@@ -1,11 +1,15 @@
 import axios from "axios"
 
+const instance = axios.create({
+	baseURL: "http://api.openweathermap.org/data/2.5"
+});
+
 const fetchWeatherCity = {
 	getWeatherCity(city: string, country: string){
-		return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}A&appid=${process.env.REACT_APP_NOT_SECRET_CODE}&units=metric`)
+		return instance.get(`/weather?q=${city},${country}A&appid=${process.env.REACT_APP_NOT_SECRET_CODE}&units=metric`)
 	},
 	findCity(letters: string){
-		return axios.get(`http://api.openweathermap.org/data/2.5/find?q=${letters}&appid=${process.env.REACT_APP_NOT_SECRET_CODE}&units=metric`)
+		return instance.get(`/find?q=${letters}&appid=${process.env.REACT_APP_NOT_SECRET_CODE}&units=metric`)
 	}
 }
 
